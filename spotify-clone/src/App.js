18a8a71434c3224151b376473701dfe313e0ecbd
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-02-07 13:52:42 -0500

    React app file strucutre

---
+++ spotify-clone/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Spotify Clone</h2>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+// acd9765734c7429dbb5235cd4c417830
