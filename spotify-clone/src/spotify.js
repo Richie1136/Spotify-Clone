37c16e371763544d2d4d19d1ddd433e51cc07931
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-02-08 13:12:31 -0500

    Created a spotify.js file which i am exporting the loginUrl

---
+++ spotify-clone/src/spotify.js
@@ -0,0 +1,17 @@
+// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+
+export const authEndpoint = "https://accounts.spotify.com/authorize"
+
+const redirectUri = 'http://localhost:3000/'
+
+const clientId = 'acd9765734c7429dbb5235cd4c417830'
+
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+]
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`
