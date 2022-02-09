d13e52ef157be947401a864aee7083d0573164e3
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-02-09 14:41:53 -0500

    Styled the spotify logo

--- spotify-clone/src/components/sidebar/Sidebar.js
+++ spotify-clone/src/components/sidebar/Sidebar.js
@@ -2,7 +2,9 @@ import './Sidebar.css'
 
 const Sidebar = () => {
   return (
-    <div className='sidebar'>Sidebar</div>
+    <div className='sidebar'>
+      <img className='sidebar-logo' src='https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg' alt='' />
+    </div>
   )
 }
 
