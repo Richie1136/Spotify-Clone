909298947084be9becf65a4cbb9c64f8c03ea8e8
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-02-09 14:49:18 -0500

    Created a sidebaroption component which I imported into the Sidebar component and changed the title prop to the different option on the sidebar

--- spotify-clone/src/components/sidebar/Sidebar.js
+++ spotify-clone/src/components/sidebar/Sidebar.js
@@ -1,9 +1,13 @@
+import SideBarOption from '../sidebaroption/SideBarOption'
 import './Sidebar.css'
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
       <img className='sidebar-logo' src='https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg' alt='' />
+      <SideBarOption title='Home' />
+      <SideBarOption title='Search' />
+      <SideBarOption title='Your Library' />
     </div>
   )
 }
